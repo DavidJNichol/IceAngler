ab12fa68b09a4e6177207015c708fc14317125e4
David Nichol <5cents87@gmail.com>
2022-04-19 18:20:04 -0500

    Adds VS

--- IceAngler/Assets/Scripts/MarineObjects/Interfaces/ISpawner.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ISpawner
-{
-    List<MarineObject> ObjectsInScene { get; set; }
-    float SpawnOffsetY { get; set; }
-}
--- IceAngler/Assets/Scripts/Utility/ITimer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ITimer
-{
-    bool TimerIsRunning { get; set; }
-    float TimeRemaining { get; set; }
-}
